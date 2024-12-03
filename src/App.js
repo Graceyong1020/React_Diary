53374ceb412fa555bdaeeb27c36b0644ab145b4e
GRACE\Baby <graceyong1020@gmail.com>
2024-12-02 21:43:01 -0800

    web storage

--- src/App.js
+++ src/App.js
@@ -6,26 +6,34 @@ import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import React, { useReducer, useRef, useEffect, useState } from "react";
 
-
-//일기 데이터 관리 State 
+//일기 데이터 관리 State
 function reducer(state, action) {
   switch (action.type) {
     case "INIT": {
       return action.data;
     }
     case "CREATE": {
+      const newState = [action.data, ...state]; //배열 만들어 변수 newState에 저장. 해당 변숫값을 localStorage에 저장후, 반환해 일기 State 업데이트
+      localStorage.setItem("diary", JSON.stringify(newState));
       return [action.data, ...state];
     }
     case "UPDATE": {
-      return state.map((it) =>
-        String(it.id) === String(action.data.id) ? action.data : it
+      const newState = state.map((it) =>
+        String(it.id) === String(action.data.id) ? { ...action.data } : it
       );
-         
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
     }
     case "DELETE": {
-      return state.filter((it) => String(it.id) !== String(action.targetId));
+      const newState = state.filter(
+        (it) => String(it.id) !== String(action.targetId)
+      );
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
     }
-    return state;
+    default: {
+      return state;
+    }
   }
 }
 
@@ -36,13 +44,24 @@ function App() {
   const [isDataLoaded, setIsDataLoaded] = useState(false); //데이터 로딩 상태
   const [data, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
-  
+
   useEffect(() => {
-    dispatch({
-      type: "INIT",
-      data: mockData,
-    });
-    setIsDataLoaded(true); //데이터 로딩 완료
+    //로컬스토리지에서 데이터 불러오기. 새로고침 시 데이터 유지
+    const rawData = localStorage.getItem("diary");
+    if (!rawData) {
+      setIsDataLoaded(true);
+      return;
+    }
+    const localData = JSON.parse(rawData);
+    if (localData.length === 0) {
+      setIsDataLoaded(true);
+      return;
+    }
+    localData.sort((a, b) => Number(b.id) - Number(a.id));
+    idRef.current = localData[0].id + 1;
+
+    dispatch({ type: "INIT", data: localData });
+    setIsDataLoaded(true);
   }, []);
 
   const onCreate = (date, content, emotionId) => {
@@ -76,50 +95,48 @@ function App() {
     dispatch({
       type: "DELETE",
       targetId,
-      });
-    };
+    });
+  };
 
-    const mockData = [
-      {
-        id: "mock1",
-        date : new Date().getTime(),
-        content: "mock1",
-        emotionId: 1
-      },
-      {
-        id: "mock2",
-        date : new Date().getTime(),
-        content: "mock2",
-        emotionId: 2
-      },
-      {
-        id: "mock3",
-        date : new Date().getTime(),
-        content: "mock3",
-        emotionId: 3
-      },
-    ];
+  const mockData = [
+    {
+      id: "mock1",
+      date: new Date().getTime(),
+      content: "mock1",
+      emotionId: 1,
+    },
+    {
+      id: "mock2",
+      date: new Date().getTime(),
+      content: "mock2",
+      emotionId: 2,
+    },
+    {
+      id: "mock3",
+      date: new Date().getTime(),
+      content: "mock3",
+      emotionId: 3,
+    },
+  ];
 
-    if (!isDataLoaded) {
-      return <div> Loading... </div>;
-    }else {
-      return (
-        <DiaryStateContext.Provider value={data}>
-          <DiaryDispatchContext.Provider
-            value={{ onCreate, onUpdate, onDelete }} >
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/diary/:id" element={<Diary />} />
-            <Route path="/edit/:id" element={<Edit />} />
-          </Routes>
-        </div>
+  if (!isDataLoaded) {
+    return <div> Loading... </div>;
+  } else {
+    return (
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/diary/:id" element={<Diary />} />
+              <Route path="/edit/:id" element={<Edit />} />
+            </Routes>
+          </div>
         </DiaryDispatchContext.Provider>
-        </DiaryStateContext.Provider>
-        
-      );
-    }
+      </DiaryStateContext.Provider>
+    );
   }
+}
 
 export default App;
